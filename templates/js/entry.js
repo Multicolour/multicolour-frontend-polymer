a753b890c8239639f4ae1b1efd5314ef5821fb7c
davemackintosh <me@davemackintosh.co.uk>
2016-01-16 18:08:38 +0000

    Working on default template.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- templates/js/entry.js
+++ templates/js/entry.js
@@ -1,16 +1,25 @@
-"use strict"
+/* global document */
 
-// Do we have native components?
-var nativeComponents = (
-  "registerElement" in document &&
-  "import" in document.createElement("link") &&
-  "content" in document.createElement("template")
-)
+/*
+ * This file checks for native web components
+ * support, if there is no support (Safari, IE)
+ * it will load the webcomponents lite polyfills.
+ */
+(function() {
+  "use strict"
 
-// If not, add the webcomponents shim.
-if (!nativeComponents) {
-  var script = document.createElement("script")
-  script.async = true
-  script.src = "./bower_components/webcomponentsjs/webcomponents-lite.min.js"
-  document.head.appendChild(script)
-}
+  // Do we have native components?
+  var nativeComponents = (
+    "registerElement" in document &&
+    "import" in document.createElement("link") &&
+    "content" in document.createElement("template")
+  )
+
+  // If not, add the webcomponents polyfill.
+  if (!nativeComponents) {
+    var script = document.createElement("script")
+    script.async = true
+    script.src = "./bower_components/webcomponentsjs/webcomponents-lite.min.js"
+    document.head.appendChild(script)
+  }
+})()
