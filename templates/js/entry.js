dc1c0db6138b2c34c0d9536cf748cdcb6ce6a559
davemackintosh <me@davemackintosh.co.uk>
2016-01-26 21:59:58 +0000

    We're coming together now.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- templates/js/entry.js
+++ templates/js/entry.js
@@ -1,25 +1,33 @@
-/* global document */
+/* global document window */
 
 /*
  * This file checks for native web components
- * support, if there is no support (Safari, IE)
- * it will load the webcomponents lite polyfills.
+ * support, if there is no support (Safari, IE, Firefox)
+ * it will load the webcomponents lite polyfill.
  */
 (function() {
   "use strict"
 
+  // The app is available everywhere.
+  window.app = document.querySelector("#app")
+
   // Do we have native components?
-  var nativeComponents = (
+  var native_components_support = (
     "registerElement" in document &&
     "import" in document.createElement("link") &&
     "content" in document.createElement("template")
   )
 
   // If not, add the webcomponents polyfill.
-  if (!nativeComponents) {
+  if (!native_components_support) {
     var script = document.createElement("script")
     script.async = true
     script.src = "./bower_components/webcomponentsjs/webcomponents-lite.min.js"
     document.head.appendChild(script)
   }
+
+  // When the app is ready, add the stuff we need like XHR headers.
+  window.app.addEventListener("dom-change", function app_ready() {
+    window.app.set("headers", { accept: "application/vnd.api+json" })
+  })
 })()
