52cf8c04a199a6601c6a6a0fd1b83d9f27725fee
davemackintosh <me@davemackintosh.co.uk>
2016-02-03 09:53:27 +0000

    Default theme.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- templates/js/entry.js
+++ templates/js/entry.js
@@ -4,12 +4,29 @@
  * This file checks for native web components
  * support, if there is no support (Safari, IE, Firefox)
  * it will load the webcomponents lite polyfill.
+ *
+ * It also sets up the app wide binding on #app and
+ * makes the app global available.
  */
 (function() {
   "use strict"
 
-  // The app is available everywhere.
-  window.app = document.querySelector("#app")
+  /**
+   * Set default headers for all XHR
+   * requests here.
+   * @return {HTMLTemplateElement}
+   */
+  function set_default_headers() {
+    // The app is available everywhere.
+    window.app = document.querySelector("#app")
+
+    // When the app is ready, add the stuff we need like XHR headers.
+    window.app.set("headers", Object.freeze({
+      accept: "application/vnd.api+json"
+    }))
+
+    return window.app
+  }
 
   // Do we have native components?
   var native_components_support = (
@@ -20,14 +37,17 @@
 
   // If not, add the webcomponents polyfill.
   if (!native_components_support) {
+    // When we're ready, set the defaults.
+    window.addEventListener("WebComponentsReady", set_default_headers)
+
+    // Add the shim.
     var script = document.createElement("script")
     script.async = true
-    script.src = "./bower_components/webcomponentsjs/webcomponents-lite.min.js"
+    script.src = "/bower_components/webcomponentsjs/webcomponents-lite.min.js"
     document.head.appendChild(script)
   }
-
-  // When the app is ready, add the stuff we need like XHR headers.
-  window.app.addEventListener("dom-change", function app_ready() {
-    window.app.set("headers", { accept: "application/vnd.api+json" })
-  })
+  else {
+    // Set the default headers.
+    set_default_headers()
+  }
 })()
