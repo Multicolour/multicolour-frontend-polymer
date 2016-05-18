60a1b8bbde5d393f4df4a4f7d237410a2455e7d5
davemackintosh <me@davemackintosh.co.uk>
2016-05-18 15:40:10 +0100

    If config doesn't exist, don't do anything.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- install.js
+++ install.js
@@ -9,60 +9,72 @@ require("colors")
 const install_to = path.resolve("../../content/frontend")
 
 // Get the user's config.
-const config_path = require.resolve("../../config.js")
-const config = require(config_path)
+const config_path = path.resolve("../../config.js")
 
-// Check if there is config for the frontend already.
-if (!config.hasOwnProperty("frontend")) {
-  config.frontend = {
-    src: `${install_to}/src`,
-    build: `${install_to}/build`
-  }
-}
-else {
-  if (!config.frontend.src) {
-    config.frontend.src = `${install_to}/src`
-  }
-
-  if (!config.frontend.build) {
-    config.frontend.build = `${install_to}/build`
-  }
-}
-
-fs.stat(config.frontend.src, (err, stats) => {
-  if (stats) {
+fs.access(config_path, fs.R_OK, err => {
+  // Error probably means the file doesn't exist.
+  if (err) {
     /* eslint-disable */
-    return console.log(`Not overwriting your theme: "${config.frontend.src}"`.blue)
+    console.info(`"${config_path}" doesn't exist, not install default theme.`)
     /* eslint-enable */
+    return false
+  }
+
+  // Get the config.
+  const config = require(config_path)
+
+  // Check if there is config for the frontend already.
+  if (!config.hasOwnProperty("frontend")) {
+    config.frontend = {
+      src: `${install_to}/src`,
+      build: `${install_to}/build`
+    }
   }
   else {
-    // Copy the template files over.
-    fs.copy(path.resolve("./templates"), config.frontend.src, err => {
-      if (err) {
-        /* eslint-disable */
-        console.error(`
-          ${"Error installing default frontend source.".underline.bold}
+    if (!config.frontend.src) {
+      config.frontend.src = `${install_to}/src`
+    }
 
-          Please copy "${path.resolve("./templates")}" to "${install_to}/src" manually.
-        `.red.bold)
-        /* eslint-enable */
-      }
-    })
-
-    // Rewrite the config file.
-    /* eslint-disable */
-    console.error(`
-      ${"Could not write to your config file.".underline.bold}
-
-      Please add:
-
-      frontend: {
-        src: "${install_to}/src",
-        build: "${install_to}/build"
-      }
-
-      to "${config_path}".
-    `.yellow)
-    /* eslint-enable */
+    if (!config.frontend.build) {
+      config.frontend.build = `${install_to}/build`
+    }
   }
+
+  fs.stat(config.frontend.src, (err, stats) => {
+    if (stats) {
+      /* eslint-disable */
+      return console.log(`Not overwriting your theme: "${config.frontend.src}"`.blue)
+      /* eslint-enable */
+    }
+    else {
+      // Copy the template files over.
+      fs.copy(path.resolve("./templates"), config.frontend.src, err => {
+        if (err) {
+          /* eslint-disable */
+          console.error(`
+            ${"Error installing default frontend source.".underline.bold}
+
+            Please copy "${path.resolve("./templates")}" to "${install_to}/src" manually.
+          `.red.bold)
+          /* eslint-enable */
+        }
+      })
+
+      // Rewrite the config file.
+      /* eslint-disable */
+      console.error(`
+        ${"Could not write to your config file.".underline.bold}
+
+        Please add:
+
+        frontend: {
+          src: "${install_to}/src",
+          build: "${install_to}/build"
+        }
+
+        to "${config_path}".
+      `.yellow)
+      /* eslint-enable */
+    }
+  })
 })
